3276aa11c7c80e2e963aef92a1af69780a64cd58
snake <adsloader99@gmail.com>
2022-02-02 12:13:22 +0900

    .

---
+++ src/DBTester.kt
@@ -0,0 +1,55 @@
+package com.psw
+
+import me.liuwj.ktorm.database.Database
+import me.liuwj.ktorm.database.asIterable
+import me.liuwj.ktorm.dsl.from
+import me.liuwj.ktorm.dsl.insert
+import me.liuwj.ktorm.dsl.select
+import me.liuwj.ktorm.schema.*
+import java.util.*
+
+// 결과에 필요한 필드만 정의
+object UserDB : Table<Nothing>("t_user") {
+    //val id = int("id").primaryKey()
+    val name = varchar("name")
+    val password = varchar("password")
+}
+
+fun testDb(){
+    val database = Database.connect("jdbc:mysql://localhost:3306/ktorm", user = "root", password = "root")
+    testDSL(database)
+    testNativeSQL(database)
+}
+
+private fun testNativeSQL(database: Database) {
+    // native sql
+    val names = database.useConnection { conn ->
+        val sql = """
+        select name from t_user
+        where name like ?
+        order by id
+    """
+
+        conn.prepareStatement(sql).use { statement ->
+            statement.setString(1, "user%")
+            statement.executeQuery().asIterable().map { it.getString(1) }
+        }
+    }
+    names.forEach { println("native : $it") }
+}
+
+private fun testDSL(database: Database) {
+    // 값 입력(DSL)
+    database.insert(UserDB) {
+        set(it.name, "user@${Calendar.getInstance().timeInMillis}".run { substring(0, length - 1) })
+        set(it.password, "${Calendar.getInstance().timeInMillis}.pswd".run { substring(0, length - 1) })
+    }
+
+    for (row in database.from(UserDB).select()) {
+        println("name => ${row[UserDB.name]} password => ${row[UserDB.password]} ")
+    }
+}
+
+fun main() {
+    testDb()
+}
