0376a2b8e0be2e8b127d46f7d0d05f33b5fbda7c
snake <adsloader99@gmail.com>
2022-02-15 13:45:51 +0900

    .

---
+++ src/DBEntity.kt
@@ -0,0 +1,13 @@
+package com.psw
+
+import me.liuwj.ktorm.schema.Table
+import me.liuwj.ktorm.schema.varchar
+
+// 필요한 필드만 정의해도 된다
+object UserDB : Table<Nothing>("t_user") {
+    //val id = int("id").primaryKey()
+    val name = varchar("name")
+    val password = varchar("password")
+}
+
+data class AccountInfo(var name : String, var passwd : String)
